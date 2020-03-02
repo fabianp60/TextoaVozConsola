c56b5c7780cbe5810a6546f394ddec0d5779f889
Fabian <fabianp60@gmail.com>
2020-03-02 09:36:49 -0500

    Agregar archivos de proyecto.

---
+++ TextoaVozConsola/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace TextoaVozConsola
+{
+    class Program
+    {
+        // https://docs.microsoft.com/es-es/azure/cognitive-services/speech-service/text-to-speech
+        // como usarlo con python
+        // https://docs.microsoft.com/es-es/azure/cognitive-services/speech-service/quickstart-python-text-to-speech
+
+        public static async Task SynthesisToSpeakerAsync()
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription("subscriptionKey", "eastus2");
+            config.SpeechSynthesisLanguage = "es-MX";
+            config.SpeechSynthesisVoiceName = "es-MX-Raul-Apollo";
+            // config.SpeechSynthesisVoiceName = "es-MX-HildaRUS";
+
+            // Creates a speech synthesizer using the default speaker as audio output.
+            using (var synthesizer = new SpeechSynthesizer(config))
+            {
+                // Receive a text from console input and synthesize it to speaker.
+                Console.WriteLine("Type some text that you want to speak...");
+                Console.Write("> ");
+                string text = Console.ReadLine();
+
+                using (var result = await synthesizer.SpeakTextAsync(text))
+                {
+                    if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                    {
+                        Console.WriteLine($"Speech synthesized to speaker for text [{text}]");
+                    }
+                    else if (result.Reason == ResultReason.Canceled)
+                    {
+                        var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            SynthesisToSpeakerAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
